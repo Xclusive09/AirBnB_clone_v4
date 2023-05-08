e1c5a3074047a43a86593ee8439795a96ce46472
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2023-05-08 14:49:46 +0100

    Code Update

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+import uuid
+from os import environ
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    cache_id = str(uuid.uuid4())
+    return render_template('0-hbnb.html', cache_id=cache_id)
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
